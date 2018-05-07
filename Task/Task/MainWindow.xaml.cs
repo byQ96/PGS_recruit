d5f5d62591995c203e2ecaa6273be220801fa863
Michał Wieczorek <michal126@hotmail.com>
2018-05-07 14:16:03 +0200

    initial commit

---
+++ Task/Task/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Task.Pages;
+
+namespace Task
+{
+    /// <summary>
+    /// Logika interakcji dla klasy MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        Imie ImiePage =  new Imie();
+        Nazwisko NazwiskoPage = new Nazwisko();
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Main.Content = ImiePage;
+        }
+
+
+    }
+}
---
+++ Task/Task/Pages/Imie.xaml.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Task
+{
+    /// <summary>
+    /// Logika interakcji dla klasy Imie.xaml
+    /// </summary>
+    public partial class Imie : Page
+    {
+        public Imie()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            
+        }
+        private void Button_Click1(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
